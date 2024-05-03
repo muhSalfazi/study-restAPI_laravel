8769fe421db9f3daf726fe1caa324625df163a44
muhamad salman fauzi <salmanfauzi0512@gmail.com>
2024-05-03 14:50:01 +0700

    update softdelete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    // protected $table = 'product';
+    protected $table = 'products';
     protected $fillable = ['product_name', 'product_type', 'product_price', 'expired_at'];
-    
+    protected $dates = ['deleted_at'];
+
+
 }
